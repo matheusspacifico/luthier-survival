bfde25cd86a43b672edd9dbe92615bdd8c0dd608
matheusspacifico <matheuspacifico44@gmail.com>
2025-12-28 18:22:19 -0300

    feat: add constants class

---
+++ core/src/main/java/io/github/matheusspacifico/utils/Constants.java
@@ -0,0 +1,14 @@
+package io.github.matheusspacifico.utils;
+
+public final class Constants {
+    // Screen
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
+
+    // Player
+    public static final int PLAYER_SIZE = 32;
+    public static final float PLAYER_SPEED = 150f;
+    public static final float PLAYER_SPRINT_SPEED = 250f;
+
+    private Constants() {}
+}
