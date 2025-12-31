d9afb2cada6898695575e6df4c97ff724ce06562
matheusspacifico <matheuspacifico44@gmail.com>
2025-12-30 23:00:13 -0300

    feat: add constants for shooting mechanic

--- core/src/main/java/io/github/matheusspacifico/utils/Constants.java
+++ core/src/main/java/io/github/matheusspacifico/utils/Constants.java
@@ -10,5 +10,17 @@ public final class Constants {
     public static final float PLAYER_SPEED = 150f;
     public static final float PLAYER_SPRINT_SPEED = 250f;
 
+    // Projectile
+    public static final int PROJECTILE_SIZE = 8;
+    public static final float PROJECTILE_SPEED = 400f;
+
+    // Weapon
+    public static final int MAGAZINE_SIZE = 6;
+    public static final float RELOAD_TIME = 1.5f;
+    public static final float FIRE_RATE = 0.15f;
+
+    // Crosshair
+    public static final int CROSSHAIR_SIZE = 16;
+
     private Constants() {}
 }
