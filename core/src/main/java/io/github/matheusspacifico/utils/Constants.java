f5e40b73389e44c370fa17fa463ab94d18600805
matheusspacifico <matheuspacifico44@gmail.com>
2025-12-31 12:43:49 -0300

    feat: add new constants for sprite scaling for higher resolutions

--- core/src/main/java/io/github/matheusspacifico/utils/Constants.java
+++ core/src/main/java/io/github/matheusspacifico/utils/Constants.java
@@ -2,17 +2,22 @@ package io.github.matheusspacifico.utils;
 
 public final class Constants {
     // Screen
-    public static final int SCREEN_WIDTH = 800;
-    public static final int SCREEN_HEIGHT = 600;
+    public static final int SCREEN_WIDTH = 1920;
+    public static final int SCREEN_HEIGHT = 1080;
+
+    // Sprite scale factor (compensates for higher resolution)
+    // At 800x600 with 32px sprites, player was ~4% of screen width
+    // At 1920x1080, we scale up to maintain similar proportion
+    public static final float SPRITE_SCALE = 2.5f;
 
     // Player
-    public static final int PLAYER_SIZE = 32;
-    public static final float PLAYER_SPEED = 150f;
-    public static final float PLAYER_SPRINT_SPEED = 250f;
+    public static final int PLAYER_SIZE = (int) (32 * SPRITE_SCALE);
+    public static final float PLAYER_SPEED = 150f * SPRITE_SCALE;
+    public static final float PLAYER_SPRINT_SPEED = 250f * SPRITE_SCALE;
 
     // Projectile
-    public static final int PROJECTILE_SIZE = 8;
-    public static final float PROJECTILE_SPEED = 400f;
+    public static final int PROJECTILE_SIZE = (int) (8 * SPRITE_SCALE);
+    public static final float PROJECTILE_SPEED = 400f * SPRITE_SCALE;
 
     // Weapon
     public static final int MAGAZINE_SIZE = 6;
@@ -20,7 +25,7 @@ public final class Constants {
     public static final float FIRE_RATE = 0.15f;
 
     // Crosshair
-    public static final int CROSSHAIR_SIZE = 16;
+    public static final int CROSSHAIR_SIZE = (int) (16 * SPRITE_SCALE);
 
     private Constants() {}
 }
