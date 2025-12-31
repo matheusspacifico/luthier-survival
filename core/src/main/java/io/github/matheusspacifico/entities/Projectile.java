6cda94f9e31c47592e9f21f02a3c7943b087e2ae
matheusspacifico <matheuspacifico44@gmail.com>
2025-12-30 23:00:35 -0300

    feat: add projectile object class

---
+++ core/src/main/java/io/github/matheusspacifico/entities/Projectile.java
@@ -0,0 +1,78 @@
+package io.github.matheusspacifico.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.github.matheusspacifico.utils.Constants;
+
+public class Projectile {
+    private float x;
+    private float y;
+    private float velocityX;
+    private float velocityY;
+    private boolean active;
+
+    private static Texture texture;
+
+    public Projectile(float startX, float startY, float targetX, float targetY) {
+        this.x = startX;
+        this.y = startY;
+        this.active = true;
+
+        float dirX = targetX - startX;
+        float dirY = targetY - startY;
+        float length = (float) Math.sqrt(dirX * dirX + dirY * dirY);
+
+        if (length > 0) {
+            this.velocityX = (dirX / length) * Constants.PROJECTILE_SPEED;
+            this.velocityY = (dirY / length) * Constants.PROJECTILE_SPEED;
+        } else {
+            this.velocityX = 0;
+            this.velocityY = Constants.PROJECTILE_SPEED;
+        }
+
+        if (texture == null) {
+            texture = new Texture("note-bullet.png");
+        }
+    }
+
+    public void update(float delta) {
+        if (!active) return;
+
+        x += velocityX * delta;
+        y += velocityY * delta;
+
+        if (x < -Constants.PROJECTILE_SIZE || x > Constants.SCREEN_WIDTH + Constants.PROJECTILE_SIZE ||
+            y < -Constants.PROJECTILE_SIZE || y > Constants.SCREEN_HEIGHT + Constants.PROJECTILE_SIZE) {
+            active = false;
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        if (!active) return;
+        batch.draw(texture, x - Constants.PROJECTILE_SIZE / 2f, y - Constants.PROJECTILE_SIZE / 2f,
+            Constants.PROJECTILE_SIZE, Constants.PROJECTILE_SIZE);
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public static void disposeTexture() {
+        if (texture != null) {
+            texture.dispose();
+            texture = null;
+        }
+    }
+}
